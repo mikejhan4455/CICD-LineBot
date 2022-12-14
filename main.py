f2eaa06abcfa68bd5128ec8923b45c87de14e9f8
Default user <default@maastrichtuniversity.nl>
2022-12-14 13:02:39 +0000

    🍺 remove env file

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import hashlib
 import hmac
 
 
-dotenv.load_dotenv()
+dotenv.load_dotenv(".env.yaml")
 line_bot_api = LineBotApi(os.getenv("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 
@@ -78,5 +78,5 @@ def callback(request):
 @handler.add(MessageEvent, message=TextMessage)
 def message_text(event):
     line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=event.message.text + 'version 2')
+        event.reply_token, TextSendMessage(text=event.message.text + "version 2")
     )
