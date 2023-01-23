1a8144d673d1a575bb5ae90cf1ec8a3af54446a2
Default user <default@maastrichtuniversity.nl>
2023-01-23 13:50:56 +0000

    :zap: Switch to Firebase_admin Package :heavy_plus_sign: Improve R/W on User data :heavy_plus_sign: Add Search Methodx :heavy_plus_sign: Add Save user function map

---
+++ FirebaseHandlerAdmin.py
@@ -0,0 +1,214 @@
+from itertools import product
+import os
+from typing import List, Optional
+
+import dotenv
+import firebase_admin
+from firebase_admin import credentials, db
+import numpy as np
+
+
+class FirebaseHandler:
+    def __init__(self, firebase_url=None) -> None:
+
+        if firebase_url:
+            self.firebase_url = firebase_url
+        else:
+            # get firebase_url from environment variables
+            dotenv.load_dotenv(".env.yaml")
+            self.firebase_url = os.getenv("FIREBASE_URL") or ""
+
+        # Exception handler
+        if not self.firebase_url:
+            raise Exception(
+                "Please specify firebase_url from parameter or with environment variable"
+            )
+
+        # Auth Step
+        self.fire_base_cert = {
+            "type": os.getenv("fire_base_cert_type"),
+            "project_id": os.getenv("fire_base_cert_project_id"),
+            "private_key_id": os.getenv("fire_base_cert_private_key_id"),
+            "private_key": os.getenv("fire_base_cert_private_key"),
+            "client_email": os.getenv("fire_base_cert_client_email"),
+            "client_id": os.getenv("fire_base_cert_client_id"),
+            "auth_uri": os.getenv("fire_base_cert_auth_uri"),
+            "token_uri": os.getenv("fire_base_cert_token_uri"),
+            "auth_provider_x509_cert_url": os.getenv(
+                "fire_base_cert_auth_provider_x509_cert_url"
+            ),
+            "client_x509_cert_url": os.getenv("fire_base_cert_client_x509_cert_url"),
+        }
+
+        # Fetch the service account key JSON file contents
+        cret = credentials.Certificate(self.fire_base_cert)
+        # Initialize the app with a service account, granting admin privileges
+        firebase_admin.initialize_app(cret, {"databaseURL": self.firebase_url})
+
+    def __read(self, user_id, node):
+        ref = db.reference(f"/users/{user_id}/{node}")
+        data = ref.get()
+        return data
+
+    def __write(self, user_id, node, data):
+        ref = db.reference(f"/users/{user_id}/{node}")
+        ref.set(data)
+
+    def __delete(self, user_id, node):
+        ref = db.reference(f"/users/{user_id}/{node}")
+        ref.delete()
+
+    def __remove_Nones(self, data):
+
+        # 3d array
+        if isinstance(data[0][0], list):
+            for i in range(len(data)):
+                for j in range(len(data[i])):
+                    data[i][j] = [d for d in data[i][j] if d != "None"]
+            return data
+
+        # 1d array
+        return [d for d in data if d != "None"]
+
+    def __add_Nones(self, data):
+        # 3d array
+        if isinstance(data[0][0], list):
+            for i in range(len(data)):
+                for j in range(len(data[i])):
+                    data[i][j] = ["None"] + data[i][j]
+            return data
+        # 1d array
+        return ["None"] + data
+
+    def read_user_data(self, user_id):
+
+        user_data = self.__read(user_id, "data")
+
+        if user_data is None:
+            self.create_user_data_list(user_id)
+
+        user_data = self.__read(user_id, "data")
+
+        return self.__remove_Nones(user_data)
+
+    def create_user_data_list(self, user_id):
+        user_data = ["None"]
+
+        self.__write(user_id, "data", user_data)
+
+    def add_user_data(self, user_id, data):
+        user_data: list = self.read_user_data(user_id)
+        user_data.extend(data)
+
+        user_data = list(dict.fromkeys(user_data))  # remove duplicate
+        user_data = self.__add_Nones(user_data)  # Recover "None" to indicate no data
+
+        self.__write(user_id, "data", user_data)
+
+    def remove_user_data(self, user_id, data: list):
+        user_data: list = self.read_user_data(user_id)
+
+        if len(data) == 0:
+            self.create_user_data_list(user_id)
+            return
+
+        for d in data:
+            if d in user_data:
+                user_data.remove(d)
+
+        user_data = self.__add_Nones(user_data)  # Recover "None" to indicate no data
+
+        self.__write(user_id, "data", user_data)
+
+    def clear_user_data(self, user_id):
+        self.__write(user_id, "data", ["None"])
+
+    def read_user_funcs_map(self, user_id):
+
+        funcs_map = self.__read(user_id, "func")
+
+        # check funcs_map initialized
+        if funcs_map is None:
+            self.create_user_funcs_map(user_id)
+
+        funcs_map = self.__read(user_id, "func")
+
+        # remove "None" from list
+        funcs_map = self.__remove_Nones(funcs_map)
+
+        return funcs_map
+
+    def create_user_funcs_map(self, user_id):
+        funcs_map = np.zeros((24, 4), object)
+
+        for i in range(funcs_map.shape[0]):
+            for j in range(funcs_map.shape[1]):
+                funcs_map[i][j] = [
+                    "None"
+                ]  # First item will remain "None" to indicate no task, prevent from empty list
+
+        print(funcs_map)
+
+        self.__write(user_id, "func", funcs_map.tolist())
+
+    def add_user_funcs_map(
+        self,
+        user_id,
+        funcs: list,
+        hours: Optional[list] = None,
+        minutes: Optional[list] = None,
+    ):
+
+        if not hours:
+            hours = [i for i in range(24)]
+        if not minutes:
+            minutes = [i for i in range(4)]
+
+        funcs_map = self.read_user_funcs_map(user_id)
+
+        for h, m in product(hours, minutes):
+            funcs_map[h][m].extend(funcs)
+            funcs_map[h][m] = list(dict.fromkeys(funcs_map[h][m]))  # remove duplicate
+
+        # add "None" to list
+        funcs_map = self.__add_Nones(funcs_map)
+
+        self.__write(user_id, "func", funcs_map)
+
+    def remove_user_funcs_map(
+        self,
+        user_id,
+        funcs: list,
+        hours: Optional[list] = None,
+        minutes: Optional[list] = None,
+    ):
+        """
+        Remove functions from user's funcs map
+
+        Args:
+            user_id (_type_): line user_id
+            funcs (list): list of functions that want to remove, e.g. ['func1', 'func2'], if empty, remove all functions
+            hours (Optional[list], optional): _description_. Defaults to None.
+            minutes (Optional[list], optional): from 0 to 3, represent 0, 15, 30, 45 . Defaults to None, represent all four.
+        """
+        if not hours:
+            hours = [i for i in range(24)]
+        if not minutes:
+            minutes = [i for i in range(4)]
+
+        funcs_map = self.read_user_funcs_map(user_id)
+
+        if len(funcs) == 0:
+            self.create_user_funcs_map(user_id)
+            return
+
+        # remove specific functions
+        for h, m, f in product(hours, minutes, funcs):
+            if f in funcs_map[h][m]:
+                funcs_map[h][m].remove(f)
+
+        # add "None" to list
+        funcs_map = self.__add_Nones(funcs_map)
+
+        self.__write(user_id, "func", funcs_map)
+
--- MessageEventHandler.py
+++ MessageEventHandler.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta, timezone
 import re
 from typing import Any
-from FirebaseHandler import FirebaseHandler
+from FirebaseHandlerAdmin import FirebaseHandler
 
 
 class MessageEventHandler:
@@ -16,8 +16,6 @@ class MessageEventHandler:
         user_data = self.get_user_data(user_id)
 
         # message route
-        if message == "date":
-            return self.date()
 
         if re.match("list", message, re.IGNORECASE):
 
@@ -32,9 +30,9 @@ class MessageEventHandler:
 
             return self.delete_data(user_id, message, user_data)
 
-        if re.match("remind", message, re.IGNORECASE):
+        if re.match("search", message, re.IGNORECASE):
 
-            return self.remind_data(user_id, message)
+            return self.search_data(user_id, message)
 
         # 其他一律存入 database
         return self.remember_data(user_id, message, user_data)
@@ -86,21 +84,25 @@ class MessageEventHandler:
         # remove keyword
         insensitive_delete = re.compile(re.escape("delete"), re.IGNORECASE)
         message = insensitive_delete.sub("", message).strip()
+        remove_data = message.split()
 
-        # if index is given, transform to text
-        if message.isdigit():
-            index = int(message)
+        for idx, data in enumerate(remove_data):
+            # if index is given, transform to text
+            if data.isdigit():
+                data = int(data)
 
-            # change index to text
-            message = user_data[index] if index < len(user_data) else message
+                # change index to text
+                remove_data[idx] = user_data[data] if data < len(user_data) else data
+
+            # if data is in user_data, add a message
+            if data in user_data:
+                reply_message.append(f"已刪除 {data}")
+
+            else:
+                reply_message.append(f"沒有 {message} 的記錄")
 
         # remove by text
-        result = self.firebase_handler.delete(user_id, message)
-
-        if result:
-            reply_message.append(f"已經刪除: {message}")
-        else:
-            reply_message.append(f"沒有 {message} 的記錄")
+        self.firebase_handler.remove_user_data(user_id, remove_data)
 
         reply_message.extend(self.list_data(user_id))
 
@@ -108,30 +110,56 @@ class MessageEventHandler:
 
     def remember_data(self, user_id, message, user_data):
         reply_message = []
-        self.firebase_handler.write(user_id, message)
+        data = message.split()
+        self.firebase_handler.add_user_data(user_id, data)
 
         # reply success
-        reply_message.append(f"已記住 {message}")
+        for d in data:
+            reply_message.append(f"已記住 {message}")
 
         # reply current database
         reply_message.extend(self.list_data(user_id))
 
         return reply_message
 
-    def remind_data(self, user_id: str, message: str):
-        # TODO：WIP
-        reply_message = []
-
-        return reply_message
-
     def search_data(self, user_id: str, message: str):
-        # TODO: WIP
+        # TODO: under Testing
         reply_message = []
+
+        # search my index or re
+        user_data = self.get_user_data(user_id)
+
+        # remove keyword "search"
+        insensitive_search = re.compile(re.escape("search"), re.IGNORECASE)
+        message = insensitive_search.sub("", message).strip()
+        message = message.split()
+
+        if not user_data:
+            reply_message.append("目前沒有資料噢")
+
+        # search by index
+        elif message[0].isdigit():
+            index = int(message[0])
+            if index < len(user_data):
+                reply_message.append(user_data[index])
+
+            else:
+                reply_message.append("沒有這個索引")
+
+        # search inline
+        else:
+            search_data = " ".join(message)
+            search_data = search_data.replace(" ", ".*")
+            search_data = f".*{search_data}.*"
+
+            for data in user_data:
+                if re.match(search_data, data, re.IGNORECASE):
+                    reply_message.append(data)
 
         return reply_message
 
     def get_user_data(self, user_id) -> list:
-        user_data: dict | None = self.firebase_handler.read(user_id)  # type: ignore
+        user_data: dict | None = self.firebase_handler.read_user_data(user_id)  # type: ignore
         user_data: list = user_data["data"] if user_data else []  # type: ignore
 
         return user_data
